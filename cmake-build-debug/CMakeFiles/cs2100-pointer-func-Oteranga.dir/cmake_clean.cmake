152f64e176947cc6b3fa2f0ab4150a8d8e2b3b61
Oteranga <40015080+Oteranga@users.noreply.github.com>
2020-04-10 19:12:05 -0500

    Environment completed

---
+++ cmake-build-debug/CMakeFiles/cs2100-pointer-func-Oteranga.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cs2100-pointer-func-Oteranga.dir/main.cpp.o"
+  "cs2100-pointer-func-Oteranga.pdb"
+  "cs2100-pointer-func-Oteranga"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cs2100-pointer-func-Oteranga.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
