919b093f3bf594c0168463e30637b97983ad0a22
Oteranga <40015080+Oteranga@users.noreply.github.com>
2020-04-12 02:22:20 -0500

    Errors fixed

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ void  _b_int_ (void * _vector_, int _i,int _k){
     // TODO:  QUE COSA PONGO AQUI?
     int *arr= reinterpret_cast<int*>(_vector_);
     if(arr[_i]>arr[_i+1]){
-        std::swap(arr[_i],arr[_i+1]);
+        swap(arr[_i],arr[_i+1]);
     }
 }
 
@@ -35,7 +35,7 @@ void  _b_char_ (void * _vector_, int _i,int _k){
 }
 template <typename T>
 void print(T arr[],size_t n){
-    for(int i=0; i<n; i++){
+    for(T i=0; i<n; i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
@@ -43,12 +43,11 @@ void print(T arr[],size_t n){
 
 int main(){
     int i = 0;
-
+    srand(time(NULL));
     //int _array_1 [5] = {2,20,7,108,9}; // RANDOMS
     int _array_1 [5];
     for(int j=0; j<5; j++) {
-        _array_1[j] = rand() % 200 + 1;
-    }
+        _array_1[j] = rand() % 200 + 1;}
     float _array_2 [5] = {4.4,5.4,9.1,7.4,2.2};
     double _array_3 [5] = {3.0,8.0,2.0,4.0,15.0};
     char _array_4 [6] = {'k','a','r','l','o','s'};
@@ -56,7 +55,6 @@ int main(){
     _bubblesort_(_array_1,_b_int_,5);
     // PRINT
     print(_array_1,5);
-    cout<<endl;
     _bubblesort_(_array_2,_b_float_,5);
     // PRINT
     _bubblesort_(_array_3,_b_double_,5);
